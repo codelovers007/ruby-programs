b922cffdc63d9739ab0f6a64111f83d0603f5231
Niraj <50963212+codelovers007@users.noreply.github.com>
2024-09-30 14:41:10 +0530

    Update kth-largest.rb

--- kth-largest.rb
+++ kth-largest.rb
@@ -7,3 +7,24 @@
 # k = 3
  
 # Output: 5
+
+def find_kth_larget_element(data, k)
+    loop do
+        swap = false
+        (data.length-1).times do |i|
+            if data[i] > data[i+1]
+                data[i], data[i+1] = data[i+1], data[i]
+                swap = true
+            end
+        end
+        break !swap
+    end 
+    data[-k]
+end
+
+# To run the inputs
+array = [2,4,1,6,5,3,7]
+k = 3
+find_kth_larget_element(array, k)
+
+
